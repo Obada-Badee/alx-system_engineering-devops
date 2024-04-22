d0bad4b2d62c19e08ebbe5598a750ee241a88366
Obada-Badee <obadabadee3@hotmail.com>
2024-04-22 23:17:43 +0300

    Fix pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 '''
-    Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.
+    Python script that, using this REST API,
+    for a given employee ID, returns information
+    about his/her TODO list progress.
 '''
 
 import requests
@@ -12,17 +14,17 @@ if __name__ == "__main__":
         url = "https://jsonplaceholder.typicode.com"
         req = requests.get(f"{url}/users/{userId}")
         name = req.json().get("name")
-        
+
         if name is not None:
             todo_req = requests.get(f"{url}/todos?userId={userId}").json()
-            at_c = len(todo_req) #at_c = all tasks count
+            at_c = len(todo_req)  # at_c = all tasks count
             completed_tasks = []
 
             for task in todo_req:
                 if task.get("completed") is True:
                     completed_tasks.append(task)
 
-            ct_c = len(completed_tasks) #ct_c == completed task count
+            ct_c = len(completed_tasks)  # ct_c == completed task count
 
             print(f"Employee {name} is done with tasks({ct_c}/{at_c}):")
 
